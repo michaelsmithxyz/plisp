54fb57764270bd9fbb9db95812bcf118686bc1b6
Michael Smith <michael@s0lder.com>
2015-11-05 15:55:56 -0500

    add source execution from files

--- plisp/__main__.py
+++ plisp/__main__.py
@@ -1,7 +1,13 @@
+import argparse
+import sys
+
 from plisp.interpreter import PLispInterpreter
 
-if __name__ == '__main__':
-    interpreter = PLispInterpreter()
+
+def setup_args(parser):
+    parser.add_argument('file', type=str, nargs='?', default=None, help="the source file to run")
+
+def repl(interpreter):
     while True:
         try:
             line = str(input("> "))
@@ -12,4 +18,28 @@ if __name__ == '__main__':
             res = interpreter.execute()
             print(res)
         except Exception as e:
-            print(str(type(e)) + ": " + str(e))
+            print(str(type(e)) + ": " + str(e), file=sys.stderr)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    setup_args(parser)
+    args = parser.parse_args()
+
+    interpreter = PLispInterpreter()
+    filename = args.file
+
+    if filename is not None:
+        try:
+            with open(filename, 'r') as source:
+                program = source.read()
+        except Exception as e:
+            print(str(e), filename=sys.stderr)
+        interpreter.load_string(program)
+        try:
+            interpreter.execute()
+        except Exception as e:
+            print(str(type(e)) + ": " + str(e), file=sys.stderr)
+    else:
+        repl(interpreter)
+
+    
